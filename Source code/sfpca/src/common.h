472ae726108f4b52865fa4a6a0946ff0134c3970
Luofeng Liao <971312990@qq.com>
2018-03-29 11:45:35 +1100

    FAIL

---
+++ Source code/sfpca/src/common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+double GAMMA = 3.7;
+arma::mat UCOEF;
+arma::vec UCONST_VEC;
+arma::mat VCOEF;
+arma::vec VCONST_VEC;
+using namespace std;
+using namespace Rcpp;
+using namespace arma;
+#endif // COMMON_H
